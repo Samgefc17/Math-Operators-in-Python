c3cdcf4ad1f4717650fa406c2ee43c73edd578ba
Samgefc17 <107057204+Samgefc17@users.noreply.github.com>
2022-06-09 15:26:03 +0100

    Add files via upload

---
+++ AddTimesDiv code mod pow and user2.py
@@ -0,0 +1,24 @@
+#User add number
+Num1 = int(input("Type a number: "))
+Num2 = int(input("Type another number: "))
+
+#Math to add num1 to num2 
+
+print(Num1 + Num2)
+
+#Math to times num1 to num2
+
+print(Num1 * Num2)
+
+#Math to divid num1 to num2
+
+print(Num1 / Num2)
+
+#Math for power of num1 to num2
+
+print (Num1 ** Num2)
+
+#Math for num 1 (mod num2)
+
+print(Num1 % Num2)
+
